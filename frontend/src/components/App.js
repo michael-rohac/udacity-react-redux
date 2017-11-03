1328fe473920eb4d67c3030d2ddc776dbfd425ea
Michal Roháč <michael.rohac@gmail.com>
2017-11-03 09:08:00 +0100

    Frontend project cleanup

---
+++ frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <h1 className="text-center">Readable Application</h1>
+
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
