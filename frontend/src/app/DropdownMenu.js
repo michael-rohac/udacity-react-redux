131d83ea42271e9e45eabb0639bb5545211c6843
Michal Roháč <michael.rohac@gmail.com>
2017-12-03 19:24:03 +0100

    Sample use of functional stateless component

--- frontend/src/app/DropdownMenu.js
+++ frontend/src/app/DropdownMenu.js
@@ -1,34 +1,29 @@
 /**
  * © 2017 Michal Rohac, All Rights Reserved.
  */
-import React, {Component} from 'react';
+import React from 'react';
 
-class DropdownMenu extends Component {
-    render() {
-        const {menu} = this.props;
-        const dropdownToggleIcon = menu.dropdownToggleIcon || 'glyphicon glyphicon-th'
-        return (
-            <div className="btn-group">
-                <a href="" className="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span className={dropdownToggleIcon}></span>
-                </a>
-                <ul className="dropdown-menu">
-                    {menu.menuItems.map(menuItem => (
-                        <li key={menuItem.id}>
-                            <a href=""
-                               onClick={e => {
-                                   e.preventDefault()
-                                   menuItem.action && menuItem.action()
-                               }}>
-                                <span className={menuItem.iconClass}></span>
-                                &nbsp;{menuItem.displayName}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        )
-    }
+export default ({menu}) => {
+    const dropdownToggleIcon = menu.dropdownToggleIcon || 'glyphicon glyphicon-th'
+    return (
+        <div className="btn-group">
+            <a href="" className="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span className={dropdownToggleIcon}></span>
+            </a>
+            <ul className="dropdown-menu">
+                {menu.menuItems.map(menuItem => (
+                    <li key={menuItem.id}>
+                        <a href=""
+                           onClick={e => {
+                               e.preventDefault()
+                               menuItem.action && menuItem.action()
+                           }}>
+                            <span className={menuItem.iconClass}></span>
+                            &nbsp;{menuItem.displayName}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
-
-export default DropdownMenu
